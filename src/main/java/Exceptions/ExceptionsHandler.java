bb92610080651c8a2e3a1ef6c11e7f05913f494e
D'Amore Lorenza <lorenzadamore@outlook.it>
2024-02-05 17:02:51 +0100

    exceptions

---
+++ src/main/java/Exceptions/ExceptionsHandler.java
@@ -0,0 +1,29 @@
+package Exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import payloads.ErrorsDTO;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class ExceptionsHandler {
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorsDTO handleBadRequest(BadRequestException ex) {
+        return new ErrorsDTO(ex.getMessage(), LocalDateTime.now());
+    }
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorsDTO handleNotFound(NotFoundException ex) {
+        return new ErrorsDTO(ex.getMessage(), LocalDateTime.now());
+    }
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorsDTO handleGenericError(Exception ex) {
+        ex.printStackTrace();
+        return new ErrorsDTO("Problema nel server", LocalDateTime.now());
+    }
+}
