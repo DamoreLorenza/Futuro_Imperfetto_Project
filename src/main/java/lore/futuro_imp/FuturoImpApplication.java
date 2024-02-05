965dc3c0c8d1b3b0570c8a71106a0d0f8d2ddc8b
D'Amore Lorenza <lorenzadamore@outlook.it>
2024-02-05 16:04:40 +0100

    Initial commit

---
+++ src/main/java/lore/futuro_imp/FuturoImpApplication.java
@@ -0,0 +1,13 @@
+package lore.futuro_imp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuturoImpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuturoImpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lore/futuro_imp/FuturoImpApplicationTests.java
@@ -0,0 +1,13 @@
+package lore.futuro_imp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuturoImpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
